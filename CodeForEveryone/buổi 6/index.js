5d8c92c78174d797c0b17a691c2e348100ecb321
HoangleRMIT <s3715120@rmit.edu.vn>
2025-10-16 20:47:18 +0700

    new

--- CodeForEveryone/buổi 6/index.js
+++ CodeForEveryone/buổi 6/index.js
@@ -1,1 +1,27 @@
-console.log("Hello world!")
+console.log("Hello world!");
+let x = 10;
+let y = 20;
+const PI = 3.14;
+console.log(x);
+console.log(y);
+console.log(PI);
+let texthello = "Xin chao den voi Mindx";
+console.log(texthello);
+let anonymousValue;
+console.log("🚀 ~ anonymousValue:", anonymousValue);
+let isRaning = true;
+console.log("🚀 ~ isRaning:", isRaning);
+console.log(typeof x);
+const testresult = 5 % 2;
+console.log(testresult);
+x += y;
+console.log(x);
+// nối chuỗi ${}
+const resultCompare = x > y;
+console.log(resultCompare);
+// x===y so sánh cả kiểu dữ liệu
+let d = 10;
+let c = 5;
+
+console.log(c > 8 || d > 8); // true, vì x > 8 đúng
+console.log(c < 8 || d > 8); // false, vì cả hai điều kiện đều sai
