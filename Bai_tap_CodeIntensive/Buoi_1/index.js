1be0ca897f5827b844a7413f959101111cdb5fc9
HoangleRMIT <s3715120@rmit.edu.vn>
2025-12-21 09:45:38 +0700

    new

--- Bai_tap_CodeIntensive/Buoi_1/index.js
+++
@@ -1,2 +0,0 @@
-import { Template_Literals as productionInfo } from "./module.js";
-console.log(productionInfo("Sách", 20000)); 
--- Bai_tap_CodeIntensive/Buoi_1/module.js
+++
@@ -1,4 +0,0 @@
-const Template_Literals = (name, price) => {
-    return `Sản phẩm ${name}, giá ${price}`
-}
-export { Template_Literals };
